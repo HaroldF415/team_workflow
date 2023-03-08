7476c115806a9c700b2fe11b654b142432022f24
Wilton <wiltoncamilol@gmail.com>
2023-03-08 13:21:10 -0500

    first commit WC

---
+++ src/Nav.js
@@ -0,0 +1,17 @@
+
+export default function Nav() {
+  return (
+    <header>
+      <article>
+        <h1>
+          
+            Screen<span>Views</span>
+          
+        </h1>
+      </article>
+      <nav>
+      
+      </nav>
+    </header>
+  );
+}
